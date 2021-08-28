3c03f214687e6bfae3e7ffe91206f77341cca9c9
pranjali0527 <68731759+pranjali0527@users.noreply.github.com>
2021-08-28 23:48:18 +0530

    Add files via upload

---
+++ Task 2/src/Extra Assests/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <div class="contain">
+          <div class="col">
+            <h1></h1>
+            <ul>
+             
+              
+            </ul>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Footer;
